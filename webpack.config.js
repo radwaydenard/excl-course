4f0d8415e8be979d3623ae496f42d0a2db0e1346
Anton Zotau <radwaydenard@yahoo.com>
2022-02-04 18:09:17 +0300

    Install plugins

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: './index.js',
+    output: {
+        filename: "bundle.[hash].js",
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: "index.html"
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/favicon.ico'),
+                    to: path.resolve(__dirname, 'dist')
+                },
+            ],
+        }),
+    ],
+}
